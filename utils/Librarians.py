f44fdc1b27613ec0b2b8a0110af24359514f3ea7
HamzLDN <75676416+HamzLDN@users.noreply.github.com>
2025-03-10 19:51:28 +0000

    Update Librarians.py

---
+++ utils/Librarians.py
@@ -0,0 +1,9 @@
+class Librarian(User):
+    def __init__(self, name):
+        super().__init__(name: str, user_type="Librarian")
+
+    def manage_books(self, library, command, book=None):
+        if command == "add": library.add_book(book)
+        elif command == "remove": library.remove_book(book)
+        elif command == "update": library.update_book(book)
+        else: print("Invalid action.")
