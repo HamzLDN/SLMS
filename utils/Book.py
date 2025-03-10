d2d82fff20216608272043f2d1db37d1d115b710
HamzLDN <75676416+HamzLDN@users.noreply.github.com>
2025-03-10 19:21:30 +0000

    Update Book.py
    Added getter and setter methods

--- utils/Book.py
+++ utils/Book.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import timedelta
 class Book:
     def __init__(self,  book_title, author):
         self._title = book_title
@@ -11,3 +13,29 @@ class Book:
             print("The book '{}' has been reserved.".format(self._title))
         else:
             print("The Book '{}' is not available for reservation.".format(self._title))
+
+    def set_due_date(self, days=14):
+        self._due_date = datetime.now() + timedelta(days=days)
+
+    @property
+    def author(self):
+        return self._author #Read author Only
+        
+    @property
+    def status(self):
+        return self._status # read status
+
+    @status.setter
+    def status(self, value):
+        self._status = value #able to modify value
+
+    @property
+    def due_date(self):
+        return self._due_date
+        
+    @property
+    def title(self):
+        return self._title #Read title Only
+
+
+
