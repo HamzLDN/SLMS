f077104bf070f4c18f56c8eef4fa49b3a1c2f034
HamzLDN <75676416+HamzLDN@users.noreply.github.com>
2025-03-10 19:28:48 +0000

    Update Member.py

--- utils/Member.py
+++ utils/Member.py
@@ -1,3 +1,12 @@
+class Member(User):
+    def __init__(self, name):
+        super().__init__(name, user_type="Member")
 
-class Librarians:
-    def __init__(self, )
+    def search_book(self, library, author_name=None book_title=None):
+        books_found = library.search_books(title, author_name)
+        if books_found:
+            print("We've found a few books for you")
+            for book in books_found:
+                print("{} by {}: {}".format(book.book_title, book.author_name, book.status))
+        else:
+            print("No books available at the moment.\nPlease try again later")
