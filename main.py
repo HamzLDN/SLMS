6054907bea46281d309d16b06fe6d1846f7d57af
HamzLDN <75676416+HamzLDN@users.noreply.github.com>
2025-03-10 02:06:37 +0000

    Update main.py
    Created user class

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import json
 from src.handleuser.signup import Signup
 from src.handleuser.login import Login
 
-Signup("First name", "email@gmail.com", "admin", "secret").register()
-session = Login("admin", "secret".encode()).access()
-print(session)
+# Signup("First name", "email@gmail.com", "admin", "secret").register()
+# session = Login("admin", "secret".encode()).access()
+# print(session)
+
+class User:
+    def __init__(self, name, user_type):
+        self._name = name
+        self._user_type = user_type
+        self._borrowed_books = []
